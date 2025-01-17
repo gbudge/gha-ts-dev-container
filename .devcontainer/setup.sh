34effad973ae8b17759e782c6d2d6dfb51962681
bud003 <gbudge@gmail.com>
2025-01-18 09:57:18 +1100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Initialize Node.js project
 if [ ! -f package.json ]; then
   npm init -y
+
+  # Set a valid name for the project
+  sed -i '' 's/"name": ".*"/"name": "gha-pr-validator"/' package.json
 fi
 
 # Install GitHub Action dependencies
@@ -11,8 +14,5 @@ npm install @actions/core @actions/github
 # Install dev dependencies
 npm install --save-dev typescript jest @types/jest ts-jest eslint prettier
 
-# Copy templates to project root
-cp -r .devcontainer/templates/* .
-
-# Initialize Jest
-npx ts-jest config:init
+# Initialize Jest (if provided jest.config.js is inadequate)
+# npx ts-jest config:init
